3f3cf01c7d0600967a823233625d1da67c7555ba
ElizabethAdhiambo <lizeuniteradhiambo@gmail.com>
2022-11-16 13:07:37 +0300

    changes

--- src/components/Interest.jsx
+++ src/components/Interest.jsx
@@ -5,7 +5,7 @@ function Interest() {
         <div className="person"> 
             <h1 className='persona-header'>Interest</h1>
             <p className='persona-para'>
-                
+                Am interested in how the Internet Of Things (IOT) and Internet Of Everything (IOE) works. Building something out of nothing through using my own mind is amaizing.
             </p>
         </div>
     )
